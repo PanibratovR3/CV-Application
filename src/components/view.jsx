a37bec5d335bf0044f33999413800cef2be9c02a
PanibratovR3 <roman.panibratov@gmail.com>
2025-07-08 09:58:23 +0300

    Added styles for view page.

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -15,64 +15,66 @@ function View({
         </button>
       </div>
       <h1>Your CV</h1>
-      <div className="header">{personalData.name}</div>
-      <p>
-        <b>Email:</b> <i>{personalData.email}</i>
-      </p>
-      <p>
-        <b>Phone number:</b> <i>{personalData.phoneNumber}</i>
-      </p>
-      <h2>Education</h2>
-      <ul>
-        {educationData.map((educationDataItem) => {
-          return (
-            <li key={educationDataItem.id}>
-              <b>{educationDataItem.schoolName}</b> (
-              {educationDataItem.titleOfStudy})
-              <p>
-                {educationDataItem.from.slice(
-                  0,
-                  educationDataItem.from.indexOf("-")
-                )}
-                -
-                {educationDataItem.to.slice(
-                  0,
-                  educationDataItem.to.indexOf("-")
-                )}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
-      <h2>Practical Experience</h2>
-      <ul>
-        {practicalExperienceData.map((practicalExperienceItem) => {
-          return (
-            <li key={practicalExperienceItem.id}>
-              <p>
-                <b>{practicalExperienceItem.positionTitle}</b> at{" "}
-                <i>{practicalExperienceItem.companyName}</i>
-              </p>
-              <p>
-                <i>{practicalExperienceItem.responsibilities}</i>
-              </p>
-              <p>
-                {practicalExperienceItem.from.slice(
-                  0,
-                  practicalExperienceItem.from.indexOf("-")
-                )}{" "}
-                -{" "}
-                {practicalExperienceItem.workingThereFlag
-                  ? "present"
-                  : practicalExperienceItem.to.slice(
-                      0,
-                      practicalExperienceItem.to.indexOf("-")
-                    )}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="view">
+        <div className="header">{personalData.name}</div>
+        <p className="view-email">
+          <b>Email:</b> <i>{personalData.email}</i>
+        </p>
+        <p className="view-phone">
+          <b>Phone number:</b> <i>{personalData.phoneNumber}</i>
+        </p>
+        <h2>Education</h2>
+        <ul className="view-education">
+          {educationData.map((educationDataItem) => {
+            return (
+              <li key={educationDataItem.id}>
+                <b>{educationDataItem.schoolName}</b> (
+                {educationDataItem.titleOfStudy})
+                <p>
+                  {educationDataItem.from.slice(
+                    0,
+                    educationDataItem.from.indexOf("-")
+                  )}
+                  -
+                  {educationDataItem.to.slice(
+                    0,
+                    educationDataItem.to.indexOf("-")
+                  )}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+        <h2>Practical Experience</h2>
+        <ul className="view-practical-experience">
+          {practicalExperienceData.map((practicalExperienceItem) => {
+            return (
+              <li key={practicalExperienceItem.id}>
+                <p>
+                  <b>{practicalExperienceItem.positionTitle}</b> at{" "}
+                  <i>{practicalExperienceItem.companyName}</i>
+                </p>
+                <p>
+                  <i>{practicalExperienceItem.responsibilities}</i>
+                </p>
+                <p>
+                  {practicalExperienceItem.from.slice(
+                    0,
+                    practicalExperienceItem.from.indexOf("-")
+                  )}{" "}
+                  -{" "}
+                  {practicalExperienceItem.workingThereFlag
+                    ? "present"
+                    : practicalExperienceItem.to.slice(
+                        0,
+                        practicalExperienceItem.to.indexOf("-")
+                      )}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
